a495890ed31901004a1c514e528e3e21b7310f94
jiangzhifang <931430472@qq.com>
2020-10-14 18:55:34 +0800

    v0.4

--- jzf/PostInstall.sh
+++ jzf/PostInstall.sh
@@ -7,6 +7,8 @@ cp /root/jzfUbuntu1804/jzf/tbclimits.conf /etc/security/limits.d/
 cp /root/jzfUbuntu1804/jzf/99-tbcsysctl.conf /etc/sysctl.d/
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cp /root/jzfUbuntu1804/jzf/sources.list /etc/apt/
+sudo apt-get update 
+sudo apt install gnupg -y
 sudo wget -O - http://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install megacli
